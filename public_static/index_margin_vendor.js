509d24c877833451c0d83a0573c164783605ccee
Pankaj Kudawala <pankajkudawala144@gmail.com>
2018-12-31 02:50:44 +0530

    Add functionality for setting margin

--- public_static/index_margin_vendor.js
+++ public_static/index_margin_vendor.js
@@ -1,10 +1,6 @@
 var db = firebase.firestore();
 
 
-function setMargin() {
-   
-}
-
 (function setOperators() {
     db.collection("Operators").get().then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
@@ -20,3 +16,36 @@ function setMargin() {
         });
     });
 })();
+
+function setMargin() {
+    var margin_vendor = document.getElementById("vendorMargin").value;
+    var selected_operator;
+    var selected_vendor;
+    var dbref = db.collection("vMargins").doc(selected_vendor).collection("Operators").doc(selected_operator);
+    if($('#radio_active_margin').prop('checked')){
+        dbref.set({
+            margin:margin_vendor,
+            status:"active"
+        })
+        .then(function() {
+            console.log("Document successfully written!");
+            // window.location.href="manage-message.html"
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+    }else if($('#radio_inactive_margin').prop('checked')){
+        dbref.set({
+            margin:margin_vendor,
+            status:"inactive"
+        })
+        .then(function() {
+            console.log("Document successfully written!");
+            // window.location.href="manage-message.html"
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+    }
+
+}
