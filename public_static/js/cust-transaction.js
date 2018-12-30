8639c151772c280b01f82dfa2abc0ddc7878aab0
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2018-12-31 04:31:02 +0530

    Add customer transaction functionality

---
+++ public_static/js/cust-transaction.js
@@ -0,0 +1,41 @@
+var db = firebase.firestore();
+
+(function displayTable(){
+    var dbRefOperators = db.collection("Transaction");
+  
+    dbRefOperators.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            var dbRef = dbRefOperators.doc(doc.id).collection("t_id");
+            
+            dbRef.get().then(function(querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+
+                    // Initialize object for storing fetched data
+                    var obj = {};
+
+                    // Insert data fetched from DB in customer object
+                    Object.assign(obj, doc.data());
+
+                    // Store Transaction ID in new variable which will be used for appending data to table
+                    var newRow = "<tr><td>" + doc.id + "</td>";
+                    Object.keys(obj).forEach(function(key) {
+
+                        // Store the details of transaction
+                        newRow += "<td>" + obj[key] + "</td>";
+                        
+
+                    })
+                    // Close the row
+                    newRow += "</tr>";
+                    
+
+                    // Append entries in table
+                    // $('#cust-transactions-table tbody').prepend(newRow);
+                    $('.cust-transactions-table tbody').prepend(newRow);
+                    
+                });
+            });
+        });
+    });
+
+})();
