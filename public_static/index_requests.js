7cc339fc1d7f0df64a6bd08279c6b81992436141
prince <princetjangra@gmail.com>
2019-01-02 02:52:13 +0530

    Change column name in table of operator-request.html
    Changed column name Email to Operator in table of operator-request.html

--- public_static/index_requests.js
+++ public_static/index_requests.js
@@ -1,44 +1,44 @@
 var db = firebase.firestore();
 
 (function setRequests() {
-    
+
     var dbRefrequests = db.collection("op_requests");
     //var dbRefVendors   = db.collection("Bind").doc(dbRefOperators.doc);
 
-    dbRefrequests.get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc1) {
+    dbRefrequests.get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc1) {
             var dbRefusers = db.collection("users").doc(doc1.id);
 
-            dbRefusers.get().then(function(doc) {
-                    var obj = {};
-                    // Store operator in object
-                    var id = doc.data().id;
-        
-                    // Store details in object
-                    obj.name = doc.data().name;
-                    obj.email = doc.data().email;
-                    obj.no = doc.data().number;
-        
-                    //  console.log(JSON.stringify(obj))
-                    var newRow = "<tr>";
-                    Object.keys(obj).forEach(function (key) {
-                        newRow += "<td>" + obj[key] + "</td>";
-                    })
-        
-                   // console.log(id, obj.name);
-                    newRow += "<td><button class=\"btn btn-primary approve-btn\" id=\"a" + id + "\" onclick=\"approve(this)\">Approve</button></td><td><button class=\"btn btn-primary reject-btn\" id=\"r" + id + "\" onclick=\"reject(this)\">Reject</button></td></tr>";
-        
-                    // Append entries in table
-                    $('#requests_table tbody').prepend(newRow);
-        
-                    // Using Vanilla JS
-                    // var parent = document.getElementById('bind-table').getElementsByTagName('tbody')[0];
-                    // parent.insertBefore(newRow, parent.firstChild);
-                    // })
+            dbRefusers.get().then(function (doc) {
+                var obj = {};
+                // Store operator in object
+                var id = doc.data().id;
 
-                });
+                // Store details in object
+                obj.name = doc.data().name;
+                obj.email = doc.data().email;
+                obj.no = doc.data().number;
+
+                //  console.log(JSON.stringify(obj))
+                var newRow = "<tr>";
+                Object.keys(obj).forEach(function (key) {
+                    newRow += "<td>" + obj[key] + "</td>";
+                })
+
+                // console.log(id, obj.name);
+                newRow += "<td><button class=\"btn btn-primary approve-btn\" id=\"a" + id + "\" onclick=\"approve(this)\">Approve</button></td><td><button class=\"btn btn-primary reject-btn\" id=\"r" + id + "\" onclick=\"reject(this)\">Reject</button></td></tr>";
+
+                // Append entries in table
+                $('#requests_table tbody').prepend(newRow);
+
+                // Using Vanilla JS
+                // var parent = document.getElementById('bind-table').getElementsByTagName('tbody')[0];
+                // parent.insertBefore(newRow, parent.firstChild);
+                // })
+
             });
         });
+    });
 })();
 
 function approve(item){
