56bc3a7df7a974a4157d19d3c872c8382620ded5
Pankaj Kudawala <pankajkudawala144@gmail.com>
2019-01-02 06:20:24 +0530

    requests working
    NOT Fixed
    
    not deleting temp data

--- public_static/index_requests.js
+++ public_static/index_requests.js
@@ -7,34 +7,57 @@ var db = firebase.firestore();
 
     dbRefrequests.get().then(function (querySnapshot) {
         querySnapshot.forEach(function (doc1) {
-            var dbRefusers = db.collection("users").doc(doc1.id);
 
+            var dbRefusers = db.collection("users").doc(doc1.id);
+            console.log(doc1.id)
+            // Store id in object
+            var id = doc1.id;
             dbRefusers.get().then(function (doc) {
                 var obj = {};
-                // Store operator in object
-                var id = doc.data().id;
+
+                dbRefrequests.doc(doc1.id).collection("Operators")
+                .get()
+                .then(function(querySnapshot) {
+                    querySnapshot.forEach(function(doc4) {
+                        obj.name = doc.data().name;
+                        obj.op = doc4.id;
+                        obj.no = doc.data().number;
+
+                        
+                        //  console.log(JSON.stringify(obj))
+                        var newRow = "<tr>";
+                        Object.keys(obj).forEach(function (key) {
+                            newRow += "<td>" + obj[key] + "</td>";
+                        })
+                        
+                        // Store operator whose request has has been raised by customer
+                        var opid = obj.op;
+                        
+                        // Store m/a/r + unique id_opertor // m: margin input, a: accept button, r: reject button
+                        // This will be used to append unique ids' to html elements making it easier to get their values
+                        var nid = "";
+                        nid = id + "_" + opid;
+        
+                        newRow += "<td><input type=\"number\" required min=\"0\" step=\"0.01\" placeholder=\"Enter member margin\" id=\"m" + 
+                        nid + "\"></td><td><button class=\"btn btn-primary approve-btn\" id=\"a" + 
+                        nid + "\" onclick=\"approve(this)\">Approve</button></td><td><button class=\"btn btn-primary reject-btn\" id=\"r" + 
+                        nid + "\" onclick=\"reject(this)\">Reject</button></td></tr>";
+        
+                        // Append entries in table
+                        $('#requests_table tbody').prepend(newRow);
+        
+                        // Using Vanilla JS
+                        // var parent = document.getElementById('bind-table').getElementsByTagName('tbody')[0];
+                        // parent.insertBefore(newRow, parent.firstChild);
+                        // })
+                    });
+                })
+                .catch(function(error) {
+                    console.log("Error getting documents: ", error);
+                });
 
                 // Store details in object
-                obj.name = doc.data().name;
-                obj.email = doc.data().email;
-                obj.no = doc.data().number;
-
-                //  console.log(JSON.stringify(obj))
-                var newRow = "<tr>";
-                Object.keys(obj).forEach(function (key) {
-                    newRow += "<td>" + obj[key] + "</td>";
-                })
-
-                // console.log(id, obj.name);
-                newRow += "<td><button class=\"btn btn-primary approve-btn\" id=\"a" + id + "\" onclick=\"approve(this)\">Approve</button></td><td><button class=\"btn btn-primary reject-btn\" id=\"r" + id + "\" onclick=\"reject(this)\">Reject</button></td></tr>";
-
-                // Append entries in table
-                $('#requests_table tbody').prepend(newRow);
-
-                // Using Vanilla JS
-                // var parent = document.getElementById('bind-table').getElementsByTagName('tbody')[0];
-                // parent.insertBefore(newRow, parent.firstChild);
-                // })
+               
 
             });
         });
@@ -42,15 +65,63 @@ var db = firebase.firestore();
 })();
 
 function approve(item){
-    aid = item.id.substring(1);
-    var dbref = db.collection("users").doc(aid);
+    aid = item.id.substring(1).split("_")[0];
+    var opid = item.id.split("_")[1];
+
+    // Value of margin input field
+    var minput = $('#m' + aid + '_' + opid).val();
+    //console.log(opid)
+
+
+    var dbref = db.collection("users").doc(aid).collection("operators").doc(opid);
+    var dbref2 = db.collection("mMargins").doc(aid).collection("Operators").doc(opid);
+    var dbrefT = db.collection("mMargins").doc(aid);
     var setWithMerge = dbref.set({
-        approve:"yes"
+        margin:minput
     }, { merge: true })
     .then(function() {
         console.log("Document successfully written!");
-        //UI Functions
+        //UI Functions  
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+    db.collection("users").doc(aid).get().then(function(doc) {
+        if (doc.exists) {
+            var setWithMerge2 = dbref2.set({
+                margin:minput,
+                bmargin:"0.5",
+                number:doc.data().number,
+                status:"active"
+            }, { merge: true })
+            .then(function() {
+                console.log("Document successfully written!");
+
+                db.collection("op_requests").doc(aid).collection("Operators").doc(opid).delete().then(function() {
+                    console.log("Document successfully deleted!");
+                }).catch(function(error) {
+                    console.error("Error removing document: ", error);
+                });
         
+            })
+            .catch(function(error) {
+                console.error("Error writing document: ", error);
+            });
+        } else {
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+ 
+    var setWithMerge2 = dbrefT.set({
+        temp:"data"
+    }, { merge: true })
+    .then(function() {
+        console.log("Document successfully written!");
+
+        //UI approve Functions  
+
     })
     .catch(function(error) {
         console.error("Error writing document: ", error);
@@ -58,12 +129,13 @@ function approve(item){
 }
 
 function reject(ritem){
-    rid = ritem.id.substring(1);
-    var dbref = db.collection("users").doc(rid);
+    rid = ritem.id.substring(1).split("_")[0];
+    var opid2 = ritem.id.split("_")[1];
+    var dbref = db.collection("op_requests").doc(rid).collection("Operators").doc(opid2);
     dbref.delete().then(function() {
         console.log("Document successfully deleted!");
 
-         //UI Functions
+         //UI reject Functions
 
     }).catch(function(error) {
         console.error("Error removing document: ", error);
