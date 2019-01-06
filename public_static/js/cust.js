a96a721a170fbbebf3f354b70f80a6e7b9e2cc7f
prince <prince>
2019-01-06 22:56:15 +0530

    Signout onclick button and Manage Operators onclick button
    added cust_signout() function for onclick of SIGNOUT and cust_manage_op() function for onclick of Manage Operator link of navbar.
    
    You can redirect the page while executing function cust_manage_op() after doing particular operations.

--- public_static/js/cust.js
+++ public_static/js/cust.js
@@ -1,6 +1,22 @@
+// To load navbar on every page
+
 // $(document).ready(function(){
 //     $('#nav-placeholder').load('cust-nav.html')
 // })
 $(function() {
     $('#nav-placeholder').load('cust-nav.html');
+
 });
+
+
+// ----- What happens when signout button is clicked -----
+
+function cust_signout(){
+    console.log('Signout is clicked.');
+}
+
+
+// ----- What happens when Manage Operator is clicked -----
+function cust_manage_op(){
+    console.log('Manage operator is clicked.')
+}
